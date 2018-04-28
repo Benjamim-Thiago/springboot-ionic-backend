23b4604f32612e55fc0faed2459dfde8b0ff79d4
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2018-04-28 20:52:43 -0300

    test class Category of domain

---
+++ src/main/java/com/btsistemas/cursomc/domain/Category.java
@@ -0,0 +1,64 @@
+package com.btsistemas.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Category implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    private Integer id;
+    private String description;
+
+    public Category() {
+    
+    }
+
+    public Category(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Category other = (Category) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
--- src/main/java/com/btsistemas/cursomc/resources/CategoryResource.java
+++ src/main/java/com/btsistemas/cursomc/resources/CategoryResource.java
@@ -1,5 +1,8 @@
 package com.btsistemas.cursomc.resources;
 
+import com.btsistemas.cursomc.domain.Category;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
-
+    
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Rest funcionando"; 
+    public List<Category> listar() {
+        Category c1 = new Category(1, "Informática");
+        Category c2 = new Category(2, "Escritório");
+        
+        List<Category> listing = new ArrayList<>();
+        listing.add(c1);
+        listing.add(c2);
+        return listing; 
     }
 }
