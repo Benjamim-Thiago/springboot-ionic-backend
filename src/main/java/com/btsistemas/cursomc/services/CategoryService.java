13082b2262fcd4510c28ebfed69bafee79bc4536
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2018-06-19 19:10:58 -0300

    Create method delete from categories

--- src/main/java/com/btsistemas/cursomc/services/CategoryService.java
+++ src/main/java/com/btsistemas/cursomc/services/CategoryService.java
@@ -2,11 +2,12 @@ package com.btsistemas.cursomc.services;
 
 import com.btsistemas.cursomc.domain.Category;
 import com.btsistemas.cursomc.repositories.CategoryRepository;
+import com.btsistemas.cursomc.services.exceptions.DataIntegrityException;
 import com.btsistemas.cursomc.services.exceptions.ObjectNotFoundException;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class CategoryService {
@@ -20,15 +21,25 @@ public class CategoryService {
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName()));
 
     }
-    
-    public Category insert(Category obj){
+
+    public Category insert(Category obj) {
         obj.setId(null);
         return repo.save(obj);
     }
-    
-    public Category update(Category obj){
+
+    public Category update(Category obj) {
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possivel excluir uma categoria que tenha produtos");
+        }
+
     }
 
 }
