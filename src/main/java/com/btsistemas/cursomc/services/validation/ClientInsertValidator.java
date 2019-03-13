f90304ac016bcf069393aa57ca69fa10bdee7b32
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2019-03-13 00:06:09 -0300

    create validation CPF and CNPJ

--- src/main/java/com/btsistemas/cursomc/services/validation/ClientInsertValidator.java
+++ src/main/java/com/btsistemas/cursomc/services/validation/ClientInsertValidator.java
@@ -6,8 +6,10 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.btsistemas.cursomc.domain.enums.TypeClient;
 import com.btsistemas.cursomc.dto.ClientNewDTO;
 import com.btsistemas.cursomc.resources.exception.FieldMessage;
+import com.btsistemas.cursomc.services.validation.utils.DocumentCPFCNPJ;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
 	@Override
@@ -15,9 +17,17 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 	}
 
 	@Override
-	public boolean isValid(ClientNewDTO objDto, ConstraintValidatorContext context) {
+	public boolean isValid(ClientNewDTO objDTO, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-// inclua os testes aqui, inserindo erros na lista
+		// inclua os testes aqui, inserindo erros na lista
+		if(objDTO.getTypeClient().equals(TypeClient.PESSOA_FISICA.getCode()) && !DocumentCPFCNPJ.isValidCPF(objDTO.getDocument())) {
+			list.add(new FieldMessage("document", "CPF inválido."));
+		}
+		
+		if(objDTO.getTypeClient().equals(TypeClient.PESSOA_JURÍDICA.getCode()) && !DocumentCPFCNPJ.isValidCNPJ(objDTO.getDocument())) {
+			list.add(new FieldMessage("document", "CNPJ inválido."));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
