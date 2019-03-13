1e6c16657a1ea793d86b346950e12fb18d04da6d
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2019-03-13 00:33:05 -0300

    validation custumer from insert not email repeat

--- src/main/java/com/btsistemas/cursomc/repositories/ClientRepository.java
+++ src/main/java/com/btsistemas/cursomc/repositories/ClientRepository.java
@@ -8,8 +8,10 @@ package com.btsistemas.cursomc.repositories;
 import com.btsistemas.cursomc.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
-
+	@Transactional(readOnly=true)
+	Client findByEmail(String email);
 }
--- src/main/java/com/btsistemas/cursomc/services/validation/ClientInsertValidator.java
+++ src/main/java/com/btsistemas/cursomc/services/validation/ClientInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.btsistemas.cursomc.domain.Client;
 import com.btsistemas.cursomc.domain.enums.TypeClient;
 import com.btsistemas.cursomc.dto.ClientNewDTO;
+import com.btsistemas.cursomc.repositories.ClientRepository;
 import com.btsistemas.cursomc.resources.exception.FieldMessage;
 import com.btsistemas.cursomc.services.validation.utils.DocumentCPFCNPJ;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+	@Autowired
+	private ClientRepository repo;
+
 	@Override
 	public void initialize(ClientInsert ann) {
 	}
@@ -28,6 +35,10 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 			list.add(new FieldMessage("document", "CNPJ inválido."));
 		}
 		
+		Client aux = repo.findByEmail(objDTO.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existe."));
+		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
