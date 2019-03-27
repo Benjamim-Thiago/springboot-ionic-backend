31d8df316d4da7dcca352d2181803d820ba889d4
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2019-03-27 12:29:14 -0300

    making authorize

--- src/main/java/com/btsistemas/cursomc/config/SecurityConfig.java
+++ src/main/java/com/btsistemas/cursomc/config/SecurityConfig.java
@@ -18,6 +18,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.btsistemas.cursomc.security.JWTAuthenticationFilter;
+import com.btsistemas.cursomc.security.JWTUtil;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,15 +29,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
-
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
 	private static final String[] PUBLIC_MATCHERS = { 
 			"/h2-console/**" 
 	};
 
 	private static final String[] PUBLIC_MATCHERS_GET = { 
 			"/products/**", 
-			"/categories/**",
-			"/clients/**"
+			"/categories/**"
 	};
 
 	@Override
@@ -49,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
--- src/main/java/com/btsistemas/cursomc/services/UserDetailsServiceImplementation.java
+++ src/main/java/com/btsistemas/cursomc/services/UserDetailsServiceImplementation.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.btsistemas.cursomc.domain.Client;
 import com.btsistemas.cursomc.repositories.ClientRepository;
 import com.btsistemas.cursomc.security.UserSpringSecurity;
 
+@Service
 public class UserDetailsServiceImplementation implements UserDetailsService{
 
 	@Autowired
